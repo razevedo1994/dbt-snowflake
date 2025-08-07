88f68fc759f39f7ce889f0ea60052d38bd8df670
Rodrigo <razevedo.contato@gmail.com>
2025-08-07 08:46:09 -0300

    Update customers staging

--- horrorland/models/staging/people/stg_people__customers.sql
+++ horrorland/models/staging/people/stg_people__customers.sql
@@ -1,9 +1,13 @@
 select 
     customer_id
     , age
-    , gender
+    , case 
+        when gender in ('M', 'Male') then 'M'
+        when gender in ('F', 'Female') then 'F'
+        else 'Other'
+    end as gender
     , email
     , registration_date
-    , is_vip_member
+    , to_boolean(lower(is_vip_member)) as is_vip_member
     , preferred_scare_level
 from {{ source('PEOPLE', 'CUSTOMERS') }}
